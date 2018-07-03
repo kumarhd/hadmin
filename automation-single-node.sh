2ccdc863a4ae0d14ad5af3ef8fb6d2b22b4e1f2c
kumarhd <40556362+kumarhd@users.noreply.github.com>
2018-07-03 05:45:57 +0200

    Create automation-single-node.sh

---
+++ automation-single-node.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+yum install git wget -y
+wget https://mirror.its.sfu.ca/mirror/CentOS-Third-Party/NSG/common/x86_64/jdk-8u102-linux-x64.rpm
+rpm -ivh jdk-8u102-linux-x64.rpm
+echo "export JAVA_HOME=/usr/java/default" >> /etc/profile
+echo "export PATH=\$PATH:\$JAVA_HOME/bin" >> /etc/profile
+source /etc/profile
+wget https://archive.apache.org/dist/hadoop/core/hadoop-1.2.1/hadoop-1.2.1.tar.gz
+mkdir -p /usr/hadoop/namenode /usr/hadoop/datanode /usr/hadoop/tmp /var/log/hadoop
+tar -zxvf hadoop-1.2.1.tar.gz -C /usr/hadoop/
+git clone https://github.com/kumarhd/hadmin.git
+cd hadmin
+sed -i 's/EC2HOSTNAME/'`hostname -f`'/g' core-site.xml
+sed -i 's/EC2HOSTNAME/'`hostname -f`'/g' mapred-site.xml
+cp core-site.xml /usr/hadoop/hadoop-1.2.1/conf/
+cp mapred-site.xml /usr/hadoop/hadoop-1.2.1/conf/
+echo "export JAVA_HOME=/usr/java/default" >> /usr/hadoop/hadoop-1.2.1/conf/hadoop-env.sh
+echo "export HADOOP_LOG_DIR=/var/log/hadoop" >> /usr/hadoop/hadoop-1.2.1/conf/hadoop-env.sh
+echo `hostname -f` > /usr/hadoop/hadoop-1.2.1/conf/masters
+echo `hostname -f` > /usr/hadoop/hadoop-1.2.1/conf/slaves
+chown -R hdfs:hadoop /usr/hadoop /var/log/hadoop
+echo "export HADOOP_HOME=/usr/hadoop/hadoop-1.2.1/" >> /etc/profile
+echo "export PATH=\$PATH:\$HADOOP_HOME/bin" >> /etc/profile
+source /etc/profile
+groupadd hadoop
+useradd -g hadoop hdfs
